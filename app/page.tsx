a37561a6eb9eec3d21a48ad1b7a96d0f6665f4ac
Ivan <ivan.sotodosos@flat101.es>
2024-10-31 16:52:58 +0100

    again

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@ import getNumber from "./server-actions/number";
 export default async function Home() {
   const query = await getNumber();
   return (
-    <main className="flex justify-center">Hola deploy in port {query}</main>
+    <main className="flex justify-center">Hoola deploy in port {query}</main>
   );
 }
